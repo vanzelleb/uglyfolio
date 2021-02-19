2c8311e012a14db4c7553248a8fa0b60a4d2f389
Van Zelleb <76180365+vanzelleb@users.noreply.github.com>
2021-02-19 18:36:56 +0100

    Removed PWA capabilities

--- src/main.js
+++ src/main.js
@@ -3,13 +3,5 @@ import App from "./App.vue";
 import router from "./router.js";
 import VueApexCharts from "vue3-apexcharts";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("./public/service-worker.js");
-  });
-}
-
-console.log("in main");
-
 const app = createApp(App).use(router).use(VueApexCharts);
 app.mount("#app");
