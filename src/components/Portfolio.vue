cf6ed667b10d766fe52a79473659de40e19e1985
Van Zelleb <76180365+vanzelleb@users.noreply.github.com>
2021-02-23 11:33:04 +0100

    Update Flexbox in Portfolio component

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
-    <div class="grid-row">
-      <div class="grid-item" v-for="(item, id) of assets" :key="id">
-        <fieldset @click="showDetails(item)">
-          <legend>
-            {{ item.name }}
-            <!--<span v-if="item.hasAlarm()" class="ml-2">⏰</span>
+    <div class="flexbox">
+      <fieldset
+        @click="showDetails(item)"
+        class="col"
+        v-for="(item, id) of assets"
+        :key="id"
+      >
+        <legend>
+          {{ item.name }}
+          <!--<span v-if="item.hasAlarm()" class="ml-2">⏰</span>
           <span v-if="item.forexChange" class="ml-2">💵</span>
           <span v-if="item.return" class="ml-2">💰</span>
           <span
@@ -18,19 +22,18 @@
             class="ml-2"
             >👎</span
           >-->
-          </legend>
-          <h4 v-if="!item.dateSell" style="display: inline-block">
-            {{ change([item]) >= 0 ? "Up " : "Down " }}
-            {{ toLocaleNumber(change([item]), 0) }}
-          </h4>
-          <h4 v-else style="display: inline-block">
-            {{ returns([item]) >= 0 ? "You made " : "You lost " }}
-            {{ toLocaleNumber(returns([item]), 0) }}
-          </h4>
-          <h5 style="display: inline-block">&nbsp;{{ settings.currency }}</h5>
-          <Chart :prices="item.prices" :dates="item.dates" height="60" />
-        </fieldset>
-      </div>
+        </legend>
+        <h4 v-if="!item.dateSell" style="display: inline-block">
+          {{ change([item]) >= 0 ? "Up " : "Down " }}
+          {{ toLocaleNumber(change([item]), 0) }}
+        </h4>
+        <h4 v-else style="display: inline-block">
+          {{ returns([item]) >= 0 ? "You made " : "You lost " }}
+          {{ toLocaleNumber(returns([item]), 0) }}
+        </h4>
+        <h5 style="display: inline-block">&nbsp;{{ settings.currency }}</h5>
+        <Chart :prices="item.prices" :dates="item.dates" height="60" />
+      </fieldset>
     </div>
   </div>
 </template>
