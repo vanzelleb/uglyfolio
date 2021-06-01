d869420baa39620d0ea5e630ea806c4ec0d6f92d
Van Zelleb <76180365+vanzelleb@users.noreply.github.com>
2021-06-01 17:07:04 +0200

    test backend

--- functions/finnhub.js
+++ functions/finnhub.js
@@ -1,11 +1,18 @@
 const { callApi } = require("./apiutils");
 
 exports.handler = async (event) => {
-  const url = "https://finnhub.io/api/v1/" + event.queryStringParameters.path
-  delete event.queryStringParameters.path
+  //const url = "https://finnhub.io/api/v1/" + event.queryStringParameters.path
+  return {
+    statusCode: 404,
+    header: {
+      "Access-Control-Allow-Origin": event.headers.origin
+    },
+    body: event.queryStringParameters
+  };
+  //delete event.queryStringParameters.path
 
-  event.queryStringParameters.token = process.env.VUE_APP_FINNHUB_SECRET_KEY;;
+  //event.queryStringParameters.token = process.env.VUE_APP_FINNHUB_SECRET_KEY;;
 
-  let response = await callApi(url, event);
-  return response;
+  //let response = await callApi(url, event);
+  //return response;
 };
