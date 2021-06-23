e47ac02f817ff1e70124adabc1b15256e5b7bf44
Van Zelleb <76180365+vanzelleb@users.noreply.github.com>
2021-06-23 11:01:15 +0200

    deployment config changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+/*import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {},
-  publicDir: "public"
-});
+});*/
+
+import vue from "@vitejs/plugin-vue";
+
+export default {
+  plugins: [vue()]
+};
